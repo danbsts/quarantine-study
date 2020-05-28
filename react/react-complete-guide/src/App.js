12b5fc37509246f89c39c8c344b8d4c127fb2d16
Daniel Bastos <dmrb2@cin.ufpe.br>
2020-05-28 18:22:09 -0300

    change component to use react hooks

--- react/react-complete-guide/src/App.js
+++ react/react-complete-guide/src/App.js
@@ -1,41 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-class App extends Component {
-  state = {
+const app = props => {
+  const [personsState, setPersonsState] = useState({
     persons: [
       {name: 'Max', age: 28},
       {name: 'Manu', age: 29},
       {name: 'Stephanie', age: 26}
     ]
-  }
+  });
 
-  switchNameHandler = () => {
+  const [otherState, setOtherState] = useState('some other value');
+
+  console.log(personsState);
+
+  const switchNameHandler = () => {
     // console.log('Was clicked!')
     // dont do this!!!! this.state.persons[0].name = 'Maxmilian';
-    this.setState({
+    setPersonsState({
       persons: [
         {name: 'Maximilian', age: 28},
         {name: 'Manu', age: 29},
         {name: 'Stephanie', age: 27}
       ]
-    })
-  }
+    });
+  };
 
-  render() {
-    return (
+
+  return (
       <div className="App">
         <h1>Hi im a react app</h1>
         <p>this  is working</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
       </div>
     );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'does this work?'));
-  } 
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'does this work?')); 
 }
 
-export default App;
+export default app;
