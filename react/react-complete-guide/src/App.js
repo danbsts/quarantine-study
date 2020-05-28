3ba0343bab0875e56fdf906b2046a27b64e89b85
Daniel Bastos <dmrb2@cin.ufpe.br>
2020-05-28 18:50:20 -0300

    passing method reference as parameter

--- react/react-complete-guide/src/App.js
+++ react/react-complete-guide/src/App.js
@@ -33,9 +33,16 @@ const app = props => {
         <h1>Hi im a react app</h1>
         <p>this  is working</p>
         <button onClick={switchNameHandler}>Switch Name</button>
-        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Racing</Person>
-        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+        <Person 
+          name={personsState.persons[0].name} 
+          age={personsState.persons[0].age}/>
+        <Person 
+          name={personsState.persons[1].name} 
+          age={personsState.persons[1].age}
+          click={switchNameHandler}>My Hobbies: Racing</Person>
+        <Person 
+          name={personsState.persons[2].name}  
+          age={personsState.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'does this work?')); 
