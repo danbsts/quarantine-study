f91f515602929725347538212cb9d79c10f68481
Daniel Bastos <dmrb2@cin.ufpe.br>
2020-05-28 17:50:18 -0300

    Add react create components project class

---
+++ react/react-complete-guide/src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person'
+
+class App extends Component {
+  state = {
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephanie', age: 26}
+    ]
+  }
+
+  switchNameHandler = () => {
+    // console.log('Was clicked!')
+    // dont do this!!!! this.state.persons[0].name = 'Maxmilian';
+    this.setState({
+      persons: [
+        {name: 'Maximilian', age: 28},
+        {name: 'Manu', age: 29},
+        {name: 'Stephanie', age: 27}
+      ]
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi im a react app</h1>
+        <p>this  is working</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'does this work?'));
+  } 
+}
+
+export default App;
