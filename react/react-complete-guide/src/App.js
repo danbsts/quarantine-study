30066cf90679efe445b9581aa1c9ccec98071d10
Daniel Bastos <dmrb2@cin.ufpe.br>
2020-05-28 22:08:06 -0300

    two way binding class

--- react/react-complete-guide/src/App.js
+++ react/react-complete-guide/src/App.js
@@ -11,13 +11,25 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked!')
     // dont do this!!!! this.state.persons[0].name = 'Maxmilian';
     this.setState({
       persons: [
-        {name: 'Maximilian', age: 28},
+        {name: newName, age: 28},
         {name: 'Manu', age: 29},
+        {name: 'Stephanie', age: 27}
+      ]
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    // console.log('Was clicked!')
+    // dont do this!!!! this.state.persons[0].name = 'Maxmilian';
+    this.setState({
+      persons: [
+        {name: 'Max', age: 28},
+        {name: event.target.value, age: 29},
         {name: 'Stephanie', age: 27}
       ]
     })
@@ -28,10 +40,19 @@ class App extends Component {
       <div className="App">
         <h1>Hi im a react app</h1>
         <p>this  is working</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={() => this.switchNameHandler('Maximilian')}>Switch Name</button>
+        {/* this can be inneficient /\ */}
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Max!')}
+          changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'does this work?'));
