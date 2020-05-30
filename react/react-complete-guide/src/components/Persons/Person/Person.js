ab48791e0523694ceb58633c09a15a2aa97e4f1e
Daniel Bastos <dmrb2@cin.ufpe.br>
2020-05-30 14:52:45 -0300

    react: wrapping code to return multiple elements

--- react/react-complete-guide/src/components/Persons/Person/Person.js
+++ react/react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,19 +1,21 @@
 import React, {Component} from 'react';
-import classes from './Person.css'
+import classes from './Person.css';
+import Aux from '../../../hoc/Aux';
 // if using a version of scripts > 2.0 just rename the css file to Person.module.css
 
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');    
         return (
-            <div className={classes.Person}>
+            // you can use React.Fragment - same wrapping
+            <Aux>
                 <p onClick={this.props.click}>I'm {this.props.name}, and I'm {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input 
                     type="text" 
                     onChange={this.props.changed} 
                     value={this.props.name}/>
-            </div>
+            </Aux>
         );
     }
 };
