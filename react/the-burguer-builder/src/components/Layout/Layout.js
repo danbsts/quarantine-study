5cd0d2df3f7b6fff691a713eb0da14007dba7477
Daniel Bastos <dmrb2@cin.ufpe.br>
2020-06-01 02:31:30 -0300

    react: add sidedrawer

--- react/the-burguer-builder/src/components/Layout/Layout.js
+++ react/the-burguer-builder/src/components/Layout/Layout.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import styles from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = (props) => (
-    <Aux>
-        <div>Toolbar, SideDrawerm, Backdrop</div>
-        <main className={styles.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawer 
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler}/>
+                <main className={styles.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+    )};
+}
 
 export default layout;
