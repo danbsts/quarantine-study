1ae10b314073b1ba5035ded4c2683b726d372458
Daniel Bastos <dmrb2@cin.ufpe.br>
2020-06-02 18:47:32 -0300

    react: styling links

--- react/http-reaching-the-web/src/containers/Blog/Blog.js
+++ react/http-reaching-the-web/src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
 import NewPost from "./NewPost/NewPost";
@@ -13,12 +13,21 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
-                                pathName: this.props.match.url + '/new-post',
-                                hash: '#submit',
-                                search: '?quick-submit=true'
-                            }}>New Post</Link></li>
+                            <li><NavLink
+                                to="/"
+                                exact
+                                activeClassName="my-active" // default name is "active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}>Home</NavLink></li>
+                            <li><NavLink
+                                to={{
+                                    pathname: '/new-post',
+                                    hash: '#submit',
+                                    search: '?quick-submit=true'
+                                }}
+                                exact>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
