6540b635f145043cfd02fe49c6d4705215e55e09
Daniel Bastos <dmrb2@cin.ufpe.br>
2020-06-02 16:54:23 -0300

    react: removing unused interceptors

--- react/the-burguer-builder/src/hoc/withErrorHandler/withErrorHandler.js
+++ react/the-burguer-builder/src/hoc/withErrorHandler/withErrorHandler.js
@@ -10,13 +10,19 @@ const withErrorHandler = (WrappedComponent, axios) => {
             this.state = {
                 error: null
             }
-            axios.interceptors.request.use(request => {
+            this.reqInterceptor = axios.interceptors.request.use(request => {
                 this.setState({error: null});
                 return request;
             })
-            axios.interceptors.response.use(res => res , error => {
+            this.resInterceptor = axios.interceptors.response.use(res => res , error => {
                 this.setState({error: error});
             });
+        }
+
+        componentWillUnmount() {
+            console.log("Will Unmount", this.reqInterceptor, this.resInterceptor);
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.request.eject(this.resInterceptor);
         }
 
         errorConfirmedHandler = () => {
