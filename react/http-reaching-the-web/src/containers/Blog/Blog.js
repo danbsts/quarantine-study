394d3c6c03eff8a5dc243a6352db6f609eddc8cd
Daniel Bastos <dmrb2@cin.ufpe.br>
2020-06-03 19:00:59 -0300

    react: lazy loading components

--- react/http-reaching-the-web/src/containers/Blog/Blog.js
+++ react/http-reaching-the-web/src/containers/Blog/Blog.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react';
 import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
 
 import Posts from './Posts/Posts';
-import NewPost from "./NewPost/NewPost";
-import FullPost from "./FullPost/FullPost";
+// import NewPost from "./NewPost/NewPost";
+import asyncComponent from "../../hoc/asyncComponent";
 import './Blog.css';
+
+const AsyncNewPost = asyncComponent(() => import('./NewPost/NewPost'));
 
 class Blog extends Component {
     state = {
@@ -42,7 +44,7 @@ class Blog extends Component {
                 </header>
                 <Switch>
                     {/*order here matter!!*/}
-                    {this.state.auth? <Route path="/new-post" component={NewPost}/> : null}
+                    {this.state.auth? <Route path="/new-post" component={AsyncNewPost}/> : null}
                     <Route path="/posts" component={Posts}/>
                     <Route render={() => <h1>Not found!</h1>}/>
                     {/*<Redirect from="/" to="/posts" />*/}
